bc9f1c944a55ede63ad7b167e5e4cb65da0fce15
Andrei Gavriliu <gavu@gavu.me>
2020-04-23 08:38:06 +0200

    better config management

--- oc-switcher.sh
+++ oc-switcher.sh
@@ -8,16 +8,32 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NOCOLOR='\033[0m'
 
-# check if config file exists
-if ! test -f "${OC_SWITCHER_CONFIG}"; then
-    echo -e "${YELLOW}Warning:${NOCOLOR}"
-    echo -e "    Config file ${OC_SWITCHER_CONFIG} not found"
-    echo -en "    Creating a new one ... "
-    echo "OC_REPOSITORY=\"/usr/local/bin\" # path where you store your oc clients (e.g.: $HOME/scripts)" > ${OC_SWITCHER_CONFIG}
+source ${OC_SWITCHER_CONFIG}
+
+# check if config file contains required varilables
+if [ -z "$OC_REPOSITORY" ]; then
+    echo -e "${YELLOW}WARNING:${NOCOLOR}"
+    echo -e "    OC_REPOSITORY variable is not set."
+    echo -en "    Adding default \"/usr/local/bin\" to ${OC_SWITCHER_CONFIG} ... "
+    echo "OC_REPOSITORY=\"/usr/local/bin\" # path where you store your oc clients (e.g.: $HOME/scripts)" >> ${OC_SWITCHER_CONFIG}
+    echo -e "${GREEN}done${NOCOLOR}"
+    source ${OC_SWITCHER_CONFIG}
+fi
+if [ -z "$OC_PREFIX" ]; then
+    echo -e "${YELLOW}WARNING:${NOCOLOR}"
+    echo -e "    OC_PREFIX variable is not set."
+    echo -en "    Adding default \"openshift-oc-client-\" to ${OC_SWITCHER_CONFIG} ... "
     echo "OC_PREFIX=\"openshift-oc-client-\" # how do we know how to find them (e.g: openshift-oc-client-)" >> ${OC_SWITCHER_CONFIG}
+    echo -e "${GREEN}done${NOCOLOR}"
+    source ${OC_SWITCHER_CONFIG}
+fi
+if [ -z "$OC_PATH" ]; then
+    echo -e "${YELLOW}WARNING:${NOCOLOR}"
+    echo -e "    OC_PATH variable is not set."
+    echo -en "    Adding default \"/usr/local/bin\" to ${OC_SWITCHER_CONFIG} ... "
     echo "OC_PATH=\"/usr/local/bin\" # where do we create the symlink (must be somehwere in your PATH $PATH)" >> ${OC_SWITCHER_CONFIG}
     echo -e "${GREEN}done${NOCOLOR}"
-    echo -e ""
+    source ${OC_SWITCHER_CONFIG}
 fi
 
 # source config
@@ -28,20 +44,6 @@ OC_NEW_VERSION=$1
 OC_VERSIONS=`ls ${OC_REPOSITORY} | grep ${OC_PREFIX} | sort -r`
 OC_CURRENT_VERSION=`readlink ${OC_PATH}/oc`
 OC_CHECK_TYPE=`type -t oc`
-
-# check if config file contains required varilables
-if [ -z "$OC_REPOSITORY" ]; then
-    echo -e "${RED}ERROR:${NOCOLOR}"
-    echo -e "    OC_REPOSITORY variable is not set"
-fi
-if [ -z "$OC_PREFIX" ]; then
-    echo -e "${RED}ERROR:${NOCOLOR}"
-    echo -e "    OC_PREFIX variable is not set"
-fi
-if [ -z "$OC_PATH" ]; then
-    echo -e "${RED}ERROR:${NOCOLOR}"
-    echo -e "    OC_PATH variable is not set"
-fi
 
 # basic help mesage
 function _usage {
