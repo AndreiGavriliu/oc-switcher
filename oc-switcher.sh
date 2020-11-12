c097b44b25ecfd7b75a10a666df7d5e88cfce874
Andrei Gavriliu <gavu@gavu.me>
2020-11-12 14:39:14 +0100

    added automatic download of available version

--- oc-switcher.sh
+++ oc-switcher.sh
@@ -82,11 +82,20 @@ function _get_oc_versions {
         echo -n "=> "
         echo -e "${YELLOW}${OC_VERSION}${NOCOLOR}"
     done
+    echo ""
+    echo "To download a new version run eg. <oc-switcher v4.6.1>. I will try to find it and download it for you."
 }
 
 # add alias to bash profile
 function _update_version {
     OC_REPOSITORY_FILE=${OC_REPOSITORY}/${OC_PREFIX}${OC_NEW_VERSION}
+    OC_NEW_MAJOR_VERSION=$(echo "${OC_NEW_VERSION}" | cut -d. -f1)
+    OC_NEW_VERSION=$(echo "${OC_NEW_VERSION}" | tr -d v)
+    case $(uname -s) in
+        Linux*)     OC_CLIENT_OS=linux;;
+        Darwin*)    OC_CLIENT_OS=mac;;
+        *)          OC_CLIENT_OS="UNKNOWN"
+    esac
     if [ -f "$OC_REPOSITORY_FILE" ]; then
         echo -ne "Adding version ${YELLOW}${OC_NEW_VERSION}${NOCOLOR} "
         ln -sf "${OC_REPOSITORY_FILE}" "${OC_PATH}/oc"
@@ -94,8 +103,33 @@ function _update_version {
         # enable_bash_completion
     else
         echo "Version not available."
-        echo ""
-        _get_oc_versions
+        # check if version is available online
+        RESPONSE=$(curl --write-out '%{http_code}' --silent --output /dev/null "https://mirror.openshift.com/pub/openshift-${OC_NEW_MAJOR_VERSION}/clients/ocp/${OC_NEW_VERSION}/")
+        # version is available
+        if [ "${RESPONSE}" = "200" ]
+        then
+            echo "Requested version found online"
+            # create temp folder
+            mkdir -p /tmp/oc-switcher
+            # switch to temp location
+            cd /tmp/oc-switcher || exit
+            # download file to temp location
+            echo "Downloading file"
+            curl --silent "https://mirror.openshift.com/pub/openshift-${OC_NEW_MAJOR_VERSION}/clients/ocp/${OC_NEW_VERSION}/openshift-client-${OC_CLIENT_OS}-${OC_NEW_VERSION}.tar.gz" -o "openshift-client-${OC_CLIENT_OS}-${OC_NEW_VERSION}.tar.gz"
+            # extract file
+            tar xf "/tmp/oc-switcher/openshift-client-${OC_CLIENT_OS}-${OC_NEW_VERSION}.tar.gz"
+            # rename client
+            mv "oc" "openshift-oc-client-v${OC_NEW_VERSION}"
+            # move client to repository location
+            echo "Moving oc-client to ${OC_REPOSITORY}"
+            mv "openshift-oc-client-v${OC_NEW_VERSION}" "${OC_REPOSITORY}/openshift-oc-client-v${OC_NEW_VERSION}"
+            # remove temporary folder
+            rm -fr /tmp/oc-switcher
+            echo "Done"
+        else
+            echo "No client with this version found"
+        fi
+        # echo $RESPONSE
     fi
 }
 
